2f23c48f60294964b64bbc1919b4202f7e248e87
Sosha Fedotovskaia <sinterma@gmail.com>
2025-04-24 14:51:29 +0200

    "done"

--- src/iteration2.ts
+++ src/iteration2.ts
@@ -1,5 +1,25 @@
 //
 // Iteration 2 | Functions
-//
 
+function calcMultiplication(a: number, b: number): number {
+    return a * b;
+}
 
+function isEven(n: number): boolean {
+    return n % 2 === 0;
+}
+
+function calcArrayAverage(numbersArr: number[]): number | string {
+
+    if (numbersArr.length === 0) {
+        return 'Please provide an array with at least one element';
+    }
+
+    const total = numbersArr.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    });
+
+    const average = total / numbersArr.length;
+
+    return average;
+}
